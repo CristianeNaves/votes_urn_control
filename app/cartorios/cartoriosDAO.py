9b9c78620f6d24247ad1fe9ccbed038f8e3c8f7d
Alexandre Gonçalves <xandemnf@gmail.com>
2018-06-20 12:43:08 -0300

    CartorioDAO, EstadoDAO, LogsDAO add

---
+++ app/cartorios/cartoriosDAO.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+DATABASE = './app/database.db'
+
+class Cartorios:
+    def __init__(self, nome, estado_id):
+        self.id = id
+        self.nome = nome
+        self.estado_id = estado_id
+
+def salvar_cartorios(cartorios):
+    con = sqlite3.connect(DATABASE)
+    cart = con.cursor()
+    cart.execute("INSERT INTO cartorios (id, nome, estado_id) VALUES (?,?,?)", (cartorios.id, cartorios.nome, cartorios.estado_id))
+    con.commit()
+    con.close()
+
+def buscar_cartorios():
+    cartorios = []
+    con = sqlite3.connect(DATABASE)
+    cart = con.cursor()
+    cart.execute("select * from cartorios")
+    for cartorios in cart:
+        print(cartorios)
+        cartorios = Cartorios(estado_id=cartorios[2], nome=cartorios[1])
+        cartorios.append(cartorios)
+    con.close()
+    return cartorios
+	
+	
